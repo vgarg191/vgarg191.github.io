bc9477ba6d180119e7142c83c27f4370247e4ac9
vishalGarg02 <iamvishalgarg@gmail.com>
2021-07-12 17:26:01 +0530

    code refactor

--- js/app.js
+++ js/app.js
@@ -2,42 +2,50 @@
 $(document).ready(function () {
 
   var selectedInputType = 'email';
-  $('.select-email').addClass('arrow_box');
+  var selectEmailElement = $('.select-email');
+  var selectPhoneElement = $('.select-phone');
+  var searchBoxInputElement =  $('input[type=text]');
+  
+  selectEmailElement.addClass('arrow_box');
 
-  $('.select-email').on('click', function (e) {
+  // click on email tab for entering email value in search box
+
+  selectEmailElement.on('click', function (e) {
    selectedInputType = 'email';
+
+   $('.error-msg').text('Please enter a valid email address');
    $('.search-placeholder').attr('placeholder', 'Enter an Email Address');
-   $('.select-email').addClass('arrow_box');
-   $('.select-phone').removeClass('arrow_box').addClass('not-selected');
-   /* $('.select-phone').addClass('not-selected'); */
+   selectEmailElement.addClass('arrow_box');
+  selectPhoneElement.removeClass('arrow_box').addClass('not-selected');
   });
 
-  $('.select-phone').on('click', function (e) {
+  selectPhoneElement.on('click', function (e) {
     selectedInputType = 'phoneInput';
+    $('.error-msg').text('Please enter a valid phone number');
     $('.search-placeholder').attr('placeholder', 'Enter Phone Number');
-    $('.select-phone').addClass('arrow_box');
-    $('.select-email').removeClass('arrow_box').addClass('not-selected');
+   selectPhoneElement.addClass('arrow_box');
+    selectEmailElement.removeClass('arrow_box').addClass('not-selected');
   });
 
   $('#btn-search').on('click', function (e) {
     e.preventDefault();
     localStorage.clear(); //Clears storage for next request
-    var inputValue = $('input[type="text"]').val().toLowerCase();
+    var inputValue = searchBoxInputElement.val().toLowerCase();
 
     if ( selectedInputType === 'email' && isEmailValid(inputValue)) {
         searchEmailOrNumber('email', inputValue);
     } else if ( selectedInputType === 'phoneInput' && isPhoneNumberValid(inputValue)) {
       searchEmailOrNumber('phone', inputValue);
     } else {
-        $('input[type=text]').parent().addClass('error');
+       searchBoxInputElement.parent().addClass('error');
     }
   });
 
-  $('input[type="text"]').keyup(function (event) {
-    inputValue = $('input[type=text]').val().toLowerCase();
+  searchBoxInputElement.keyup(function (event) {
+    inputValue = searchBoxInputElement.val().toLowerCase();
     
     if ( isEmailValid(inputValue) || isPhoneNumberValid(inputValue)) {
-      $('input[type=text]').parent().removeClass('error');
+     searchBoxInputElement.parent().removeClass('error');
     }
 
     keycode = (event.keyCode ? event.keyCode : event.which);
@@ -55,24 +63,30 @@ $(document).ready(function () {
       } else if (selectedInputType === 'phoneInput' && isPhoneNumberValid(inputValue)) {
         searchEmailOrNumber('phone', inputValue);
       } else {
-        $('input[type=text]').parentNode.classList.add('error');
+       searchBoxInputElement.parentNode.classList.add('error');
       }
     }
   });
 });
 
+// check if email address is vaild or not 
 function isEmailValid(email) {
  let emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
  return email.match(emailRegex);
 }
 
+// check if phone number is vaild or not 
 function isPhoneNumberValid(phoneNumber) {
   let phoneRegex = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/;
   return phoneNumber.match(phoneRegex);
 }
 
+/**
+       * Makes a request to ltv API to search an specific email address oe phone number.
+       * If there's a response, it gets stored in the local storage and redirects to results page
+       */
 function searchEmailOrNumber( numberOrEmail, inputValue) {
-    $('input[type=text]').removeClass('error');
+  $('input[type=text]').parent().removeClass('error');
     const proxyurl = '';
     const url =
       `https://ltv-data-api.herokuapp.com/api/v1/records.json?${numberOrEmail}=${inputValue}`;
@@ -80,6 +94,7 @@ function searchEmailOrNumber( numberOrEmail, inputValue) {
       .then((response) => response.text())
       .then(function (contents) {
         localStorage.setItem('userObject', contents);
+        // add loader
       $('.loading').css({'height': '100vh','display':'flex','flex-direction':'column'}).prepend('<div><img src="assets/img/loading_spinner.gif"/></div> <div>Please wait a moment...</div>');
       $('.not-loading').remove();
       $('.loading').remove();
--- js/result.js
+++ js/result.js
@@ -5,40 +5,40 @@ $(document).ready(function () {
    */
   if (window.localStorage) {
     if (localStorage.userObject) {
-      var user_object = localStorage.getItem('userObject');
-      var testVar;
+      let user_object = localStorage.getItem('userObject');
       retreivedObject = JSON.parse(user_object); //parses the retrieved object into an JSON object
-      if (JSON.stringify(retreivedObject) == "[]") {
-        $('#result-count').text("0 Results");
-        $(".result-desc").text(
-          "Try starting a new search below"
+
+      if (JSON.stringify(retreivedObject) == '[]') {
+        $('#result-count').text('0 Results');
+        $('.result-desc').text(
+          'Try starting a new search below'
         );
       } else {
-        $('#result-count').text("1 Result");
-        $("#result-subtext").html("Look at the result below to see the details of the person you’re searched for.");
-        $(".name").append(
-          retreivedObject.first_name + " " + retreivedObject.last_name
+        $('#result-count').text('1 Result');
+        $('#result-subtext').html('Look at the result below to see the details of the person you’re searched for.');
+        $('.name').append(
+          `${retreivedObject.first_name} ${retreivedObject.last_name}`
         );
         $('.user-description').append(retreivedObject.description);
-        $("#address").append("<p>" + retreivedObject.address + '</p>');
-        $(".email").append("<p>" + retreivedObject.email + "</p>");
+        $('#address').append('<p>' + retreivedObject.address + '</p>');
+        $('.email').append('<p>' + retreivedObject.email + '</p>');
 
         for (const phone_number in retreivedObject.phone_numbers) {
           phone = retreivedObject.phone_numbers[phone_number]
           formatted_phone = "(" + phone.substring(0, 3) + ") " + phone.substring(3, 6) + "-" + phone.substring(6, 10);
 
-          $(".phone-num").append(
-            "<a href=" + `tel:${phone}` + " style='display: block;color: #004A80;'>" + `${formatted_phone}` + "</a>"
+          $('.phone-num').append(
+            '<a href=' + `tel:${phone}` + " style='display: block;color: #004A80;'>" + `${formatted_phone}` + '</a>'
           );
         }
 
         for (const relative in retreivedObject.relatives) {
-          $(".relatives").append(
+          $('.relatives').append(
             "<p style='margin-bottom: 0'>" + `${retreivedObject.relatives[relative]}` + "</p>"
           );
         }
 
-        $(".result-wrap").show();
+        $('.result-wrap').show();
       }
     }
   }
